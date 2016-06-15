0fa342a88aeaac4b44243abe4107e088c64cb996
pjb4752 <pjb4752@gmail.com>
2016-06-15 17:43:14 -0400

    add destination types in instructions

--- lib/latch/instruction_set/math.rb
+++ lib/latch/instruction_set/math.rb
@@ -6,35 +6,35 @@ module Latch
       include CpuArch
 
       # register-based arithmetic
-      instruction :addr, opcode: 0x06, operands: [:rega, :regn, :regn],
+      instruction :addr, opcode: 0x06, operands: [:regd, :regn, :regn],
         operation: ->(dst, lhs, rhs) { reg[dst] = reg[lhs] + reg[rhs] },
         description: <<-DESC
           Add contents of operand registers and store result in destination
           register.
         DESC
 
-      instruction :subr, opcode: 0x07, operands: [:rega, :regn, :regn],
+      instruction :subr, opcode: 0x07, operands: [:regd, :regn, :regn],
         operation: ->(dst, lhs, rhs) { reg[dst] = reg[lhs] - reg[rhs] },
         description: <<-DESC
           Subtract contents of operand registers and store result in
           destination register.
         DESC
 
-      instruction :mulr, opcode: 0x08, operands: [:rega, :regn, :regn],
+      instruction :mulr, opcode: 0x08, operands: [:regd, :regn, :regn],
         operation: ->(dst, lhs, rhs) { reg[dst] = reg[lhs] * reg[rhs] },
         description: <<-DESC
           Multiply contents of operand registers and store result in
           destination register.
         DESC
 
-      instruction :divr, opcode: 0x09, operands: [:rega, :regn, :regn],
+      instruction :divr, opcode: 0x09, operands: [:regd, :regn, :regn],
         operation: ->(dst, lhs, rhs) { reg[dst] = reg[lhs] / reg[rhs] },
         description: <<-DESC
           Divide contents of operand registers and store result in destination
           register.
         DESC
 
-      instruction :modr, opcode: 0x0A, operands: [:rega, :regn, :regn],
+      instruction :modr, opcode: 0x0A, operands: [:regd, :regn, :regn],
         operation: ->(dst, lhs, rhs) { reg[dst] = reg[lhs] % reg[rhs] },
         description: <<-DESC
           Find remainder of division of operand registers and store result in
@@ -42,35 +42,35 @@ module Latch
         DESC
 
       # literal-based arithmetic
-      instruction :addl, opcode: 0x0B, operands: [:rega, :regn, :litn],
+      instruction :addl, opcode: 0x0B, operands: [:regd, :regn, :litn],
         operation: ->(dst, lhs, rhs) { reg[dst] = reg[lhs] + rhs },
         description: <<-DESC
           Add contents of operand register and literal and store results in
           destination register.
         DESC
 
-      instruction :subl, opcode: 0x0C, operands: [:rega, :regn, :litn],
+      instruction :subl, opcode: 0x0C, operands: [:regd, :regn, :litn],
         operation: ->(dst, lhs, rhs) { reg[dst] = reg[lhs] - rhs },
         description: <<-DESC
           Subtract contents of operand register and literal and store results
           in destination register.
         DESC
 
-      instruction :mull, opcode: 0x0D, operands: [:rega, :regn, :litn],
+      instruction :mull, opcode: 0x0D, operands: [:regd, :regn, :litn],
         operation: ->(dst, lhs, rhs) { reg[dst] = reg[lhs] * rhs },
         description: <<-DESC
           Multiply contents of operand register and literal and store results
           in destination register.
         DESC
 
-      instruction :divl, opcode: 0x0E, operands: [:rega, :regn, :litn],
+      instruction :divl, opcode: 0x0E, operands: [:regd, :regn, :litn],
         operation: ->(dst, lhs, rhs) { reg[dst] = reg[lhs] / rhs },
         description: <<-DESC
           Divide contents of operand register and literal and store results in
           destination register.
         DESC
 
-      instruction :modl, opcode: 0x0F, operands: [:rega, :regn, :litn],
+      instruction :modl, opcode: 0x0F, operands: [:regd, :regn, :litn],
         operation: ->(dst, lhs, rhs) { reg[dst] = reg[lhs] % rhs },
         description: <<-DESC
           Find remainder of division of operand register and literal and store
--- lib/latch/instruction_set/memory.rb
+++ lib/latch/instruction_set/memory.rb
@@ -5,31 +5,31 @@ module Latch
     module Memory
       include CpuArch
 
-      instruction :loadl, opcode: 0x01, operands: [:rega, :lita],
+      instruction :loadl, opcode: 0x01, operands: [:regd, :lita],
         operation: ->(dst, val) { reg[dst] = val },
         description: <<-DESC
           Load literal value into destination register.
         DESC
 
-      instruction :storg, opcode: 0x02, operands: [:glba, :rega],
+      instruction :storg, opcode: 0x02, operands: [:glbd, :rega],
         operation: ->(dst, src) { glb[dst] = reg[src] },
         description: <<-DESC
           Copy source register into destination global.
         DESC
 
-      instruction :loadg, opcode: 0x03, operands: [:rega, :glba],
+      instruction :loadg, opcode: 0x03, operands: [:regd, :glba],
         operation: ->(dst, src) { reg[dst] = glb[src] },
         description: <<-DESC
           Copy source global into destination register.
         DESC
 
-      instruction :mov, opcode: 0x04, operands: [:rega, :rega],
+      instruction :mov, opcode: 0x04, operands: [:regd, :rega],
         operation: ->(dst, src) { reg[dst] = reg[src] },
         description: <<-DESC
           Copy source register into destination register.
         DESC
 
-      instruction :movret, opcode: 0x05, operands: [:rega],
+      instruction :movret, opcode: 0x05, operands: [:regd],
         operation: ->(dst) { reg[dst] = rvl },
         description: <<-DESC
           Copy special return register into destination register.
