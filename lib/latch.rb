2f29a34bde35d93b5fc292f44d4c90a1cc4b7b35
pjb4752 <pjb4752@gmail.com>
2016-06-15 18:02:28 -0400

    remote networking code

--- lib/latch.rb
+++ lib/latch.rb
@@ -1,9 +1,6 @@
-require 'latch/network/packet_stream'
 require 'latch/cpu'
 require 'latch/cpu_monitor'
 require 'latch/version'
-
-require 'socket'
 
 module Latch
   class Vm
@@ -17,16 +14,6 @@ module Latch
     end
 
     def run
-      Socket.unix_server_socket('/tmp/latch.sock') do |server|
-        remote, addrinfo = server.accept
-        stream = Network::PacketStream.new(remote)
-
-        loop do
-          stream.pending.each do |packet|
-            handle_packet(packet)
-          end
-        end
-      end
     end
 
     private
